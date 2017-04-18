b4a58abf3f925cc1d103149bfabaac305ba1c738
caoweizhao <devimpl0706@gmail.com>
2017-04-18 21:00:08 +0800

    修复提示

--- app/src/main/java/com/example/administrator/mysensor/SnackBarUtil.java
+++ app/src/main/java/com/example/administrator/mysensor/SnackBarUtil.java
@@ -1,6 +1,7 @@
 package com.example.administrator.mysensor;
 
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -10,19 +11,39 @@ import android.view.View;
 public class SnackBarUtil {
 
     public static Snackbar mSnackbar;
+    private static Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                if (mSnackbar != null) {
+                    mSnackbar.show();
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Log.d("SnackBarUtil", "stop");
+                    mSnackbar = null;
+                }
+            }
+        }
+    };
+    private static Thread mThread;
 
     public static void showSnackBar(View view, String msg) {
         if (mSnackbar == null) {
-            mSnackbar = Snackbar.make(view, msg, Snackbar.LENGTH_SHORT);
+            mSnackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
             mSnackbar.setAction("关闭", new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mSnackbar.dismiss();
+                    mThread.interrupt();
                 }
             });
+            mThread = new Thread(mRunnable);
+            mThread.start();
         } else {
             mSnackbar.setText(msg);
         }
-        mSnackbar.show();
     }
 }
